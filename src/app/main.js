a741f7f47a9fc9380ffea48f0b5abb731853086c
brandonpayton <brandon@happycode.net>
2012-12-01 12:45:25 -0700

    Add basic game loop

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,21 @@
-define([], function() {
-    alert('stuff');
+require(['frozen/GameCore'], function(GameCore){
+  // game state
+  var x = 0;
+  var y = 0;
+
+  //setup a GameCore instance
+  var game = new GameCore({
+    canvasId: 'canvas',
+    draw: function(context){
+      context.clearRect(0, 0, this.width, this.height);
+      context.fillRect(x, y, 50, 50);
+    },
+    update: function(millis){
+      x += 1;
+      y += 1;
+    }
+  });
+
+  //launch the game!
+  game.run();
 });
--- src/app/run.js
+++ src/app/run.js
@@ -12,7 +12,7 @@ require({
     packages: [
         // If you are registering a package that has an identical name and location, you can just pass a string
         // instead, and it will configure it using that string for both the "name" and "location" properties. Handy!
-        'dojo',
+        'frozen',
         'app'
     ],
 
