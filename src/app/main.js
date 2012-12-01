20810256ff54e133a98ecb27ed11a58ccb91c0d9
brandonpayton <brandon@happycode.net>
2012-12-01 12:47:03 -0700

    Fix indent

--- src/app/main.js
+++ src/app/main.js
@@ -1,21 +1,21 @@
 require(['frozen/GameCore'], function(GameCore){
-  // game state
-  var x = 0;
-  var y = 0;
+    // game state
+    var x = 0;
+    var y = 0;
 
-  //setup a GameCore instance
-  var game = new GameCore({
-    canvasId: 'canvas',
-    draw: function(context){
-      context.clearRect(0, 0, this.width, this.height);
-      context.fillRect(x, y, 50, 50);
-    },
-    update: function(millis){
-      x += 1;
-      y += 1;
-    }
-  });
+    //setup a GameCore instance
+    var game = new GameCore({
+        canvasId: 'canvas',
+        draw: function(context){
+            context.clearRect(0, 0, this.width, this.height);
+            context.fillRect(x, y, 50, 50);
+        },
+        update: function(millis){
+            x += 1;
+            y += 1;
+        }
+    });
 
-  //launch the game!
-  game.run();
+    //launch the game!
+    game.run();
 });
