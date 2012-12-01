7520b68bfaf8e0e5bce662bfe57b169cc25f8dd3
brandonpayton <brandon@happycode.net>
2012-12-01 13:09:32 -0700

    Add obligatory gnome

--- src/app/main.js
+++ src/app/main.js
@@ -1,18 +1,24 @@
-require(['frozen/GameCore'], function(GameCore){
-    // game state
+require([ 'frozen/GameCore', 'frozen/ResourceManager' ], function(GameCore, ResourceManager){
     var x = 0;
     var y = 0;
+    var speed = 1;
+
+    //setup a ResourceManager to use in the game
+    var rm = new ResourceManager();
+    var backImg = rm.loadImage('app/resources/images/GameBack.png');
+    var gnome = rm.loadImage('app/resources/images/gnome.png');
 
     //setup a GameCore instance
     var game = new GameCore({
         canvasId: 'canvas',
+        resourceManager: rm,
         draw: function(context){
-            context.clearRect(0, 0, this.width, this.height);
-            context.fillRect(x, y, 50, 50);
+            context.drawImage(backImg, 0, 0, this.width, this.height);
+            context.drawImage(gnome, x, y);
         },
         update: function(millis){
-            x += 1;
-            y += 1;
+            x += speed;
+            y += speed;
         }
     });
 
